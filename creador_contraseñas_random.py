65729ae0f8c9b55da8190fb1bef9254d087af9be
OriginalKenoo <kenfco_05@outlook.com>
2025-06-07 12:06:45 -0600

    Actualización: Le puse validaciones y poder salir del programa o seguir si gusta el usario

--- creador_contraseñas_random.py
+++ creador_contraseñas_random.py
@@ -7,7 +7,7 @@ def generador_contraseña(cantidadNum, cantidadCarac, cantidadEspecial):
     caracteres.extend(caracteres_mayusuclas)
     especiales = ["$","/","@","-","%","&",":",";","!"] 
     contraseña = [] # creamos una lista donde estará la contraseña
-    for i in range(1, cantidadNum): # una ciclo que se repita las veces que se desea escoger la cantidad de numeros que solicita el usuario
+    for i in range(1, cantidadNum+1): # una ciclo que se repita las veces que se desea escoger la cantidad de numeros que solicita el usuario
         contraseña.append(random.choice(numeros)) # se elije un numero random de la lista y se agrega a la lista contraseña
     
     
@@ -23,20 +23,49 @@ def generador_contraseña(cantidadNum, cantidadCarac, cantidadEspecial):
     random.shuffle(contraseña) 
 
     contraseña_real = "".join(contraseña)
-    print(contraseña_real)
+    
     return contraseña_real
     
+def programa():
+    while True:
+        while True:
+            try:
+                cantidad_num = int(input("Ingresa la cantidad de numeros que desea en su contraseña: ")) # decimos al usuario la cantidad de numeros en su contraseña
+                break
+            except ValueError:
+                print("Ingresa solo valores númericos enteros")
+        while True:
+            try:
+                cantidad_caracteres = int(input("Ingresa la cantidad de carácteres que desea en su contraseña: ")) 
+                break
+            except ValueError:
+                print("Ingresa solo valores númericos enteros")
+        while True:
+            try:
+                cantidad_especiales = int(input("Ingresa la cantidad de carácteres especiales que desea en su contraseña: ")) 
+                break
+            except ValueError:
+                print("Ingresa solo valores númericos enteros")
+        contraseña = generador_contraseña(cantidad_num, cantidad_caracteres, cantidad_especiales)
+        print(f"La contraseña es {contraseña}")
+        
+        while True:
+            try:
+                option = int(input("\n¿Desea continuar?\n1.-Si\n2.-No\n"))
+            except ValueError:
+                print("Ingresa un valor númerico")
+            if option == 1:
+                break
+            elif option ==2:
+                return
+            else:
+                print("Ingresa un valor especifícado\n")
+            
+programa()
+            
+            
 
 
-
-
-
-print("Bienvenido al generador de contraseñas randoms")
-cantidad_num = int(input("Ingresa la cantidad de numeros que desea en su contraseña: ")) # decimos al usuario la cantidad de numeros en su contraseña
-cantidad_caracteres = int(input("Ingresa la cantidad de carácteres que desea en su contraseña: ")) 
-cantidad_especiales = int(input("Ingresa la cantidad de carácteres especiales que desea en su contraseña: ")) 
-contraseña = generador_contraseña(cantidad_num, cantidad_caracteres, cantidad_especiales)
-print(f"La contraseña es {contraseña}")
 
 
 
