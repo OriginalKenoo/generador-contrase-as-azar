73eec5a98a4c801a4947aad21468b760a35fd29f
OriginalKenoo <kenfco_05@outlook.com>
2025-06-07 11:12:35 -0600

    Primer commit: generador de contraseñas al azar

---
+++ creador_contraseñas_random.py
@@ -0,0 +1,42 @@
+import random # importamos random
+
+def generador_contraseña(cantidadNum, cantidadCarac, cantidadEspecial):
+    numeros = ["0","1","2","3","4","5","6","7","8","9"] 
+    caracteres= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"] 
+    caracteres_mayusuclas = [elemento.upper() for elemento in caracteres]
+    caracteres.extend(caracteres_mayusuclas)
+    especiales = ["$","/","@","-","%","&",":",";","!"] 
+    contraseña = [] # creamos una lista donde estará la contraseña
+    for i in range(1, cantidadNum): # una ciclo que se repita las veces que se desea escoger la cantidad de numeros que solicita el usuario
+        contraseña.append(random.choice(numeros)) # se elije un numero random de la lista y se agrega a la lista contraseña
+    
+    
+    for i in range(1, cantidadCarac+1): # una ciclo que se repita las veces que se desea escoger la cantidad de caracteres que solicita el usuario
+        contraseña.append(random.choice(caracteres))
+        
+        
+    for i in range(1, cantidadEspecial+1): 
+        contraseña.append(random.choice(especiales))
+    
+        
+
+    random.shuffle(contraseña) 
+
+    contraseña_real = "".join(contraseña)
+    print(contraseña_real)
+    return contraseña_real
+    
+
+
+
+
+
+print("Bienvenido al generador de contraseñas randoms")
+cantidad_num = int(input("Ingresa la cantidad de numeros que desea en su contraseña: ")) # decimos al usuario la cantidad de numeros en su contraseña
+cantidad_caracteres = int(input("Ingresa la cantidad de carácteres que desea en su contraseña: ")) 
+cantidad_especiales = int(input("Ingresa la cantidad de carácteres especiales que desea en su contraseña: ")) 
+contraseña = generador_contraseña(cantidad_num, cantidad_caracteres, cantidad_especiales)
+print(f"La contraseña es {contraseña}")
+
+
+
